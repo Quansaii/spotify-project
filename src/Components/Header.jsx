24d1bb5c299b940bfbbd67f2f175c4546b643aa8
Sayasat Kuandyk <sayasat614@gmail.com>
2026-03-16 13:18:24 +0500

    Added few changes in Header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,16 +4,17 @@ export function Header() {
   return (
     <>
       <div className="w-full h-14 bg-black flex justify-between items-center px-8">
-        <div className="w-8">
+        <div className="w-8 cursor-pointer">
           <img
             src="https://storage.googleapis.com/pr-newsroom-wp/1/2023/05/Spotify_Primary_Logo_RGB_White.png"
             alt="Spotify"
             className="w-full"
           />
         </div>
+
         <div>
           <div className="w-120 flex gap-4">
-            <button className="w-12 bg-[#1f1f1f] flex justify-center items-center rounded-full px-2 py-2">
+            <button className="w-12 bg-[#1f1f1f] flex justify-center items-center rounded-full px-2 py-2 cursor-pointer">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="30"
@@ -23,23 +24,23 @@ export function Header() {
                 <path
                   fill="#fff"
                   d="M6 19h3.692v-5.077q0-.343.233-.575q.232-.233.575-.233h3q.343 0 .576.233q.232.232.232.575V19H18v-8.692q0-.154-.067-.28t-.183-.22L12.366 5.75q-.154-.134-.366-.134t-.365.134L6.25 9.808q-.115.096-.183.22t-.067.28zm-1 0v-8.692q0-.384.172-.727t.474-.565l5.385-4.078q.423-.323.966-.323t.972.323l5.385 4.077q.303.222.474.566q.172.343.172.727V19q0 .402-.299.701T18 20h-3.884q-.344 0-.576-.232q-.232-.233-.232-.576v-5.076h-2.616v5.076q0 .344-.232.576T9.885 20H6q-.402 0-.701-.299T5 19m7-6.711"
-                  stroke-width="0.5"
+                  strokeWidth="0.5"
                   stroke="#fff"
                 />
               </svg>
             </button>
             
-            <div class="flex items-center gap-3 bg-[#222] border border-white/20 rounded-full px-4 py-2 w-full max-w-2xl focus-within:border-white/50 transition-all">
+            <div className="flex items-center gap-3 bg-[#222] border border-white/20 rounded-full px-4 py-2 w-full max-w-2xl focus-within:border-white/50 transition-all">
               <svg
-                class="w-5 h-5 text-gray-400"
+                className="w-5 h-5 text-gray-400"
                 fill="none"
                 stroke="currentColor"
                 viewBox="0 0 24 24"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                   d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
                 />
               </svg>
@@ -47,30 +48,53 @@ export function Header() {
               <input
                 type="text"
                 placeholder="Что хочешь включить?"
-                class="bg-transparent border-none outline-none flex-1 text-white placeholder-gray-500 text-sm flex items-center"
+                className="bg-transparent border-none outline-none flex-1 text-white placeholder-[#b3b3b3] text-sm flex items-center focus-within:border-2"
               />
 
-              <div class="h-6 w-px bg-gray-700 mx-1"></div>
+              <div className="h-6 w-px bg-gray-700 mx-1"></div>
 
-              <button class="text-gray-400 hover:text-white transition-colors">
+              <button className="text-[#b3b3b3] hover:text-white transition-colors">
                 <svg
-                  class="w-5 h-5"
+                  className="w-5 h-5"
                   fill="none"
                   stroke="currentColor"
                   viewBox="0 0 24 24"
                 >
                   <path
                     d="M20 7H4V17C4 18.1046 4.89543 19 6 19H18C19.1046 19 20 18.1046 20 17V7Z"
-                    stroke-width="2"
+                    strokeWidth="2"
                   />
                   <path
                     d="M16 7V5C16 3.89543 15.1046 3 14 3H10C8.89543 3 8 3.89543 8 5V7"
-                    stroke-width="2"
+                    strokeWidth="2"
                   />
                 </svg>
               </button>
             </div>
+
           </div>
+
+        </div>
+
+        <div className=" flex justify-center items-center gap-2 font-bold text-[#b3b3b3] text-sm transition-transform duration-50 ease-in-out hover:text-white hover:scale-105 cursor-pointer">
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 1200 1200" stroke="currentColor" className="cursor-pointer hover:text-white/10 hover:fill-current">
+            <path fill="#b3b3b3"  d="M600 0C268.63 0 0 268.63 0 600s268.63 600 600 600s600-268.631 600-600S931.369 0 600 0m0 1069.565c-259.37 0-469.565-210.261-469.565-469.565S340.63 130.435 600 130.435S1069.565 340.696 1069.565 600S859.369 1069.565 600 1069.565m117.392-720.652H482.608v266.739H335.87L600 864.13l264.13-248.478H717.391z" />
+          </svg>
+
+          Установить приложение
+        </div>
+
+        <div className="flex justify-center items-center gap-2">
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" stroke="currentColor" className="cursor-pointer hover:text-white/10 hover:fill-current">
+            <path fill="#fff" d="M5 18h14v-6.969C19 7.148 15.866 4 12 4s-7 3.148-7 7.031zm7-16c4.97 0 9 4.043 9 9.031V20H3v-8.969C3 6.043 7.03 2 12 2M9.5 21h5a2.5 2.5 0 0 1-5 0" />
+          </svg>
+
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" stroke="currentColor" className="cursor-pointer hover:text-white/10 hover:fill-current">
+            <g fill="none" fill-rule="evenodd">
+              <path d="m12.594 23.258l-.012.002l-.071.035l-.02.004l-.014-.004l-.071-.036q-.016-.004-.024.006l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427q-.004-.016-.016-.018m.264-.113l-.014.002l-.184.093l-.01.01l-.003.011l.018.43l.005.012l.008.008l.201.092q.019.005.029-.008l.004-.014l-.034-.614q-.005-.019-.02-.022m-.715.002a.02.02 0 0 0-.027.006l-.006.014l-.034.614q.001.018.017.024l.015-.002l.201-.093l.01-.008l.003-.011l.018-.43l-.003-.012l-.01-.01z" />
+              <path fill="#fff" d="M12 12c1.873 0 3.57.62 4.815 1.487c1.183.825 2.185 2.051 2.185 3.37c0 .724-.309 1.324-.796 1.77c-.458.421-1.056.694-1.672.88C15.301 19.88 13.68 20 12 20s-3.301-.12-4.532-.493c-.616-.186-1.214-.459-1.673-.88C5.31 18.182 5 17.582 5 16.858c0-1.319 1.002-2.545 2.185-3.37C8.43 12.62 10.127 12 12 12m0 2c-1.44 0-2.743.48-3.67 1.127c-.989.69-1.33 1.392-1.33 1.73c0 .304.352.494.672.614l.205.07l.17.052c.94.284 2.32.407 3.953.407c1.508 0 2.799-.105 3.728-.344l.304-.087l.19-.06c.343-.117.778-.314.778-.652s-.341-1.04-1.33-1.73C14.744 14.481 13.44 14 12 14m7-1c1.044 0 1.992.345 2.693.833c.64.447 1.307 1.19 1.307 2.096c0 1.335-1.297 1.813-2.463 1.98l-.3.037l-.289.025l-.138.008c.122-.345.19-.72.19-1.122a3.8 3.8 0 0 0-.107-.888c.386-.03.703-.08.939-.151c.104-.032.01-.13-.1-.215l-.107-.078l-.076-.051a2.7 2.7 0 0 0-.995-.418c-.38-.76-.964-1.418-1.586-1.943A4.8 4.8 0 0 1 19 13M5 13q.537.002 1.032.113c-.622.525-1.206 1.183-1.586 1.943a2.7 2.7 0 0 0-.995.418l-.128.088c-.127.092-.276.22-.155.256c.236.071.553.122.94.151a3.7 3.7 0 0 0-.108.888c0 .402.068.777.19 1.122l-.28-.02l-.296-.03c-1.202-.147-2.614-.607-2.614-2c0-.905.666-1.649 1.307-2.096A4.76 4.76 0 0 1 5 13m13.5-6a2.5 2.5 0 1 1 0 5a2.5 2.5 0 0 1 0-5m-13 0a2.5 2.5 0 1 1 0 5a2.5 2.5 0 0 1 0-5M12 3a4 4 0 1 1 0 8a4 4 0 0 1 0-8m6.5 6a.5.5 0 1 0 0 1a.5.5 0 0 0 0-1m-13 0a.5.5 0 1 0 0 1a.5.5 0 0 0 0-1M12 5a2 2 0 1 0 0 4a2 2 0 0 0 0-4" />
+            </g>
+          </svg>
         </div>
       </div>
     </>
